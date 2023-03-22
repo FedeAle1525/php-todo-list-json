18b5dd85fe78625488e109bf9a647ba56e44f542
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-22 15:07:49 +0100

    FIRST COMMIT

---
+++ server.php
@@ -0,0 +1,28 @@
+<?php
+
+$tasks = [
+  [
+    "text" => "Prepararsi per la lezione",
+    "done" => true
+  ],
+
+  [
+    "text" => "Andare in Banca",
+    "done" => true
+  ],
+
+  [
+    "text" => "Fare la spesa",
+    "done" => true
+  ],
+
+  [
+    "text" => "Fare esercizione pomeridiano",
+    "done" => false
+  ],
+
+  [
+    "text" => "Preparare la cena",
+    "done" => false
+  ]
+];
