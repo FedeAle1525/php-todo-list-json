d33d0e37d9c3ddc41204d67a840f6427c8ca5f57
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-22 15:54:37 +0100

    JS create method for request Server(PHP)

--- main.js
+++ main.js
@@ -3,7 +3,32 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      title: 'ToDo List JSON'
+      title: 'ToDo List JSON',
+
+      todoList: [],
     }
+  },
+
+  methods: {
+
+    // Metodo per fare chiamata al Server (PHP) e recuperare i data della risposta
+    fetchTodoList() {
+      axios.get('./server.php')
+        .then((res) => {
+          this.todoList = res.data;
+          console.log(this.todoList);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.todoList = [];
+        })
+    }
+
+  },
+
+  mounted() {
+    this.fetchTodoList();
   }
+
+
 }).mount('#app')
